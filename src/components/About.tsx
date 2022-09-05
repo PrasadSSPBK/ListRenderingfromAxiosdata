3096e14847f8e93afd7bd5a887af9cb78685e8a9
PrasadSSPBK <prasadsspbk@gmail.com>
2022-09-05 16:13:12 +0530

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,18 @@
+
 import React from 'react'
 
 interface Iprops{
 
 }
 
+
+
 const About:React.FC<Iprops>=()=>{
     return (
         <div className='container'>
             <div className="row">
                 <div className="col">
+                
                     <h1 className='h3 text-center text-success fw-bold display-3'>About</h1>
                 </div>
             </div>
